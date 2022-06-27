57a8ba93678a76bcda2b272fb3d5d35eeffd389c
Ghinet <65913071+Ghinet@users.noreply.github.com>
2022-06-27 22:55:25 +0200

    TrisGame
    I metodi per cambiare il valore del check sembrano un po' ripetitivi e superflui, ci sarà una soluzione migliore che non trovo adesso... 
    Un'altra classe per gestire esclusivamente le vincite poteva esser una buona idea

---
+++ TrisGame/src/trisGame/trisMain.java
@@ -0,0 +1,69 @@
+package trisGame;
+
+import java.util.Scanner;
+
+public class trisMain {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		GridPrint game = new GridPrint();
+		int round = 0;
+		Boolean check = true;
+
+		game.gridCreate();
+
+		System.out.println("Benvenuto al gioco del tris!\n\nGiocatore X, inserisci il tuo nickname:\n");
+		String nickX = scan.nextLine();
+		System.out.println("\nGiocatore O, inserisci il tuo nickname:\n");
+		String nickO = scan.nextLine();
+
+		System.out.println("\nBen fatto, che vinca il migliore\n");
+		game.printGrid();
+
+		//TODO far chiudere il programma dopo un metodo checWin
+		
+		
+		do {
+
+			if (round % 2 == 0) {
+				game.handleRound(nickX, "x");	
+				
+				game.checkWinLine(nickX, "x", check);
+				game.checkWinColomn(nickX, "x", check);
+				game.checkWinDiagonal1(nickX, "x", check);
+				game.checkWinDiagonal2(nickX, "x", check);
+				
+				check = game.checkWinLine(nickX, "x", check);
+				check = game.checkWinColomn(nickX, "x", check);
+				check = game.checkWinDiagonal1(nickX, "x", check);
+				check = game.checkWinDiagonal2(nickX, "x", check);
+				
+				round++;
+				
+				
+			} else {
+				game.handleRound(nickO, "o");
+				
+				game.checkWinLine(nickO, "o", check);
+				game.checkWinColomn(nickO, "o", check);
+				game.checkWinDiagonal1(nickO, "o", check); 
+				game.checkWinDiagonal2(nickO, "o", check);
+				
+				check = game.checkWinLine(nickO, "o", check);
+				check = game.checkWinColomn(nickO, "o", check);
+				check = game.checkWinDiagonal1(nickO, "o", check);
+				check = game.checkWinDiagonal2(nickO, "o", check);
+				
+				round++;
+				
+			}
+		} while (check && round != 9);
+		
+		if(round == 9) {
+			System.out.println("Partita conclusa");
+		}
+
+		
+	}
+
+}
